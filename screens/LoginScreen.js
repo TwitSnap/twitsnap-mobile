177da387a029e24ce021b55f8f243c3414edc665
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-09-28 16:01:16 -0300

    style: Re-indento

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -11,215 +11,215 @@ import { auth } from '../firebaseConfig';
 WebBrowser.maybeCompleteAuthSession();
 
 const LoginScreen = () => {
-  const navigation = useNavigation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: '450665613455-ui2sreo4d8uf4m4jjqqqskbqpb6q3sr3.apps.googleusercontent.com',
-    redirectUri: 'https://twitsnap-57128.firebaseapp.com/__/auth/handler',
-    scopes: ['profile', 'email'],
-  });
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '450665613455-ui2sreo4d8uf4m4jjqqqskbqpb6q3sr3.apps.googleusercontent.com',
+        redirectUri: 'https://twitsnap-57128.firebaseapp.com/__/auth/handler',
+        scopes: ['profile', 'email'],
+    });
 
-   
-useEffect(() => {
-  if (response && response.type === 'success') {
-    const { authentication } = response;
 
-    if (authentication && authentication.idToken) {
-      handleGoogleLogin(authentication.idToken);
-    } else {
-      console.error("Authentication object is missing idToken");
-      Alert.alert("Authentication Error", "Unable to retrieve authentication token.");
-    }
-  } else if (response && response.type === 'error') {
-    console.error("Google Auth Error:", response.error);
-    Alert.alert("Authentication Error", "An error occurred during authentication.");
-  }
-}, [response]);
+    useEffect(() => {
+        if (response && response.type === 'success') {
+            const { authentication } = response;
 
- const handleGoogleLogin = async (idToken) => {
-    try {
-      const credential = GoogleAuthProvider.credential(idToken);
+            if (authentication && authentication.idToken) {
+                handleGoogleLogin(authentication.idToken);
+            } else {
+                console.error("Authentication object is missing idToken");
+                Alert.alert("Authentication Error", "Unable to retrieve authentication token.");
+            }
+        } else if (response && response.type === 'error') {
+            console.error("Google Auth Error:", response.error);
+            Alert.alert("Authentication Error", "An error occurred during authentication.");
+        }
+    }, [response]);
 
-      const firebaseUser = await signInWithCredential(auth, credential);
-      
+    const handleGoogleLogin = async (idToken) => {
+        try {
+            const credential = GoogleAuthProvider.credential(idToken);
 
-      const token = await firebaseUser.user.getIdToken(); 
-      const res = await fetch('https://twitsnap-user-api.onrender.com/api/v1/register/google', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token }), 
-      });
+            const firebaseUser = await signInWithCredential(auth, credential);
 
-      const data = await res.json();
 
-      if (data.success) {
-        navigation.reset({
-          index: 0,
-          routes: [{ name: 'WelcomeScreen' }],
-        });
-      } else {
-        Alert.alert("Google Login failed", "Please try again.");
-      }
-    } catch (error) {
-       console.error("Error during Google login:", error);
-      Alert.alert("Error", error.message);
-    }
-  };
+            const token = await firebaseUser.user.getIdToken();
+            const res = await fetch('https://twitsnap-user-api.onrender.com/api/v1/register/google', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ token }),
+            });
 
-  const handleLogin = async () => {
-    setEmailError(email === "");
-    setPasswordError(password === "");
+            const data = await res.json();
 
-    if (!email || !password) {
-      return;
-    }
+            if (data.success) {
+                navigation.reset({
+                    index: 0,
+                    routes: [{ name: 'WelcomeScreen' }],
+                });
+            } else {
+                Alert.alert("Google Login failed", "Please try again.");
+            }
+        } catch (error) {
+            console.error("Error during Google login:", error);
+            Alert.alert("Error", error.message);
+        }
+    };
 
-    if (email === "hacker") {
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'WelcomeScreen' }],
-      });
-      return;
-    }
+    const handleLogin = async () => {
+        setEmailError(email === "");
+        setPasswordError(password === "");
 
-    setIsLoading(true);
+        if (!email || !password) {
+            return;
+        }
 
-    try {
-      const result = await LoginHandler(email, password);
+        if (email === "hacker") {
+            navigation.reset({
+                index: 0,
+                routes: [{ name: 'WelcomeScreen' }],
+            });
+            return;
+        }
 
-      if (result === 0) {
-        navigation.reset({
-          index: 0,
-          routes: [{ name: 'WelcomeScreen' }],
-        });
-      } else {
-        Alert.alert("Login failed", "Login unsuccessful. Please verify your email and password.");
-      }
+        setIsLoading(true);
 
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error.message);
-      setIsLoading(false);
-    }
-  };
+        try {
+            const result = await LoginHandler(email, password);
 
-  const handleRegister = () => {
-    navigation.navigate('RegisterScreen');
-  };
+            if (result === 0) {
+                navigation.reset({
+                    index: 0,
+                    routes: [{ name: 'WelcomeScreen' }],
+                });
+            } else {
+                Alert.alert("Login failed", "Login unsuccessful. Please verify your email and password.");
+            }
 
-  const handleForgotPassword = () => {
-    navigation.navigate('ForgotPasswordScreen');
-  };
+            setIsLoading(false);
+        } catch (error) {
+            console.error(error.message);
+            setIsLoading(false);
+        }
+    };
 
-  return (
-    <View style={styles.container}>
-      <Image source={require('../assets/logo.png')} style={styles.image} />
-      <Title style={styles.title}>TwitSnap</Title>
-      <Paragraph style={styles.subtitle}>Please enter your credentials</Paragraph>
-      <Card style={styles.card}>
-        <Card.Content>
-          <TextInput
-            style={styles.input}
-            label="Email"
-            value={email}
-            onChangeText={setEmail}
-            theme={{ colors: { primary: '#1E88E5' } }}
-            error={emailError}
-            autoCapitalize={'none'}
-          />
-          <HelperText type="error" visible={emailError}>
-            Email is required
-          </HelperText>
-          <Divider style={styles.divider} />
-          <TextInput
-            style={styles.input}
-            label="Password"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-            theme={{ colors: { primary: '#1E88E5' } }}
-            error={passwordError}
-            autoCapitalize={'none'}
-          />
-          <HelperText type="error" visible={passwordError}>
-            Password is required
-          </HelperText>
-          <Divider style={styles.divider} />
-          <CustomButton title="Login" onPress={handleLogin} loading={isLoading} />
-          <Button
-            mode="contained"
-            onPress={() => {
-             promptAsync();
-            }}          
-            disabled={!request}
-            style={styles.googleButton}
-          >
-            Login with Google
-          </Button>
-          <Button mode="text" onPress={handleRegister} style={styles.registerButton}>
-            Don't have an account? Register
-          </Button>
-          <Button mode="text" onPress={handleForgotPassword} style={styles.forgotPasswordButton}>
-            Forgot Password?
-          </Button>
-        </Card.Content>
-      </Card>
-    </View>
-  );
+    const handleRegister = () => {
+        navigation.navigate('RegisterScreen');
+    };
+
+    const handleForgotPassword = () => {
+        navigation.navigate('ForgotPasswordScreen');
+    };
+
+    return (
+        <View style={styles.container}>
+            <Image source={require('../assets/logo.png')} style={styles.image} />
+            <Title style={styles.title}>TwitSnap</Title>
+            <Paragraph style={styles.subtitle}>Please enter your credentials</Paragraph>
+            <Card style={styles.card}>
+                <Card.Content>
+                    <TextInput
+                        style={styles.input}
+                        label="Email"
+                        value={email}
+                        onChangeText={setEmail}
+                        theme={{ colors: { primary: '#1E88E5' } }}
+                        error={emailError}
+                        autoCapitalize={'none'}
+                    />
+                    <HelperText type="error" visible={emailError}>
+                        Email is required
+                    </HelperText>
+                    <Divider style={styles.divider} />
+                    <TextInput
+                        style={styles.input}
+                        label="Password"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        theme={{ colors: { primary: '#1E88E5' } }}
+                        error={passwordError}
+                        autoCapitalize={'none'}
+                    />
+                    <HelperText type="error" visible={passwordError}>
+                        Password is required
+                    </HelperText>
+                    <Divider style={styles.divider} />
+                    <CustomButton title="Login" onPress={handleLogin} loading={isLoading} />
+                    <Button
+                        mode="contained"
+                        onPress={() => {
+                            promptAsync();
+                        }}
+                        disabled={!request}
+                        style={styles.googleButton}
+                    >
+                        Login with Google
+                    </Button>
+                    <Button mode="text" onPress={handleRegister} style={styles.registerButton}>
+                        Don't have an account? Register
+                    </Button>
+                    <Button mode="text" onPress={handleForgotPassword} style={styles.forgotPasswordButton}>
+                        Forgot Password?
+                    </Button>
+                </Card.Content>
+            </Card>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 16,
-    backgroundColor: '#E3F2FD',
-  },
-  image: {
-    width: 300,
-    height: 300,
-    alignSelf: 'center',
-    marginBottom: 20,
-  },
-  title: {
-    marginBottom: 16,
-    textAlign: 'center',
-    color: '#0D47A1',
-  },
-  subtitle: {
-    marginBottom: 24,
-    textAlign: 'center',
-    color: '#0D47A1',
-  },
-  card: {
-    padding: 16,
-    elevation: 4,
-    backgroundColor: '#d4e6f1',
-  },
-  input: {
-    marginBottom: 12,
-    backgroundColor: '#FFFFFF',
-  },
-  googleButton: {
-    marginTop: 16,
-    backgroundColor: '#db4a39',
-  },
-  registerButton: {
-    marginTop: 12,
-  },
-  forgotPasswordButton: {
-    marginTop: 12,
-  },
-  divider: {
-    marginVertical: 8,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 16,
+        backgroundColor: '#E3F2FD',
+    },
+    image: {
+        width: 300,
+        height: 300,
+        alignSelf: 'center',
+        marginBottom: 20,
+    },
+    title: {
+        marginBottom: 16,
+        textAlign: 'center',
+        color: '#0D47A1',
+    },
+    subtitle: {
+        marginBottom: 24,
+        textAlign: 'center',
+        color: '#0D47A1',
+    },
+    card: {
+        padding: 16,
+        elevation: 4,
+        backgroundColor: '#d4e6f1',
+    },
+    input: {
+        marginBottom: 12,
+        backgroundColor: '#FFFFFF',
+    },
+    googleButton: {
+        marginTop: 16,
+        backgroundColor: '#db4a39',
+    },
+    registerButton: {
+        marginTop: 12,
+    },
+    forgotPasswordButton: {
+        marginTop: 12,
+    },
+    divider: {
+        marginVertical: 8,
+    },
 });
 
 export default LoginScreen;
