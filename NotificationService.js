44565bb21faa79da15ebc75bb58fd2bf91b45867
theo jorge <theojorge2001@gmail.com>
2024-12-06 18:08:11 -0300

    cambios de notificacion

--- NotificationService.js
+++ NotificationService.js
@@ -1,17 +1,54 @@
-import messaging from "@react-native-firebase/messaging";
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
-// Función para configurar notificaciones
+// Función para configurar Firebase Messaging y notificaciones locales
+const createNotificationChannel = () => {
+  PushNotification.createChannel(
+    {
+      channelId: 'default-channel', // ID del canal
+      channelName: 'Default Channel', // Nombre del canal
+      channelDescription: 'A channel for general notifications', // Descripción del canal
+      soundName: 'default', // Sonido por defecto
+      importance: PushNotification.Importance.HIGH, // Alta prioridad
+      vibrate: true, // Vibrar
+    },
+    (created) => console.log(`Channel created: ${created}`)
+  );
+};
+
 export const configureNotifications = () => {
-  // Solicitar permisos de notificación (solo en iOS)
+
+  createNotificationChannel();
+
   messaging().requestPermission();
 
-  // Escuchar notificaciones cuando la app está en primer plano
+  // Manejar mensajes cuando la app está en primer plano
   messaging().onMessage(async (remoteMessage) => {
-    console.log("FCM Message Data:", remoteMessage.data);
+    console.log('FCM Message Data:', remoteMessage);
+
+    // Mostrar notificación local si hay datos de notificación
+    if (remoteMessage.notification) {
+      PushNotification.localNotification({
+        channelId: "default-channel",
+        title: remoteMessage.notification.title,
+        message: remoteMessage.notification.body,
+        data: remoteMessage.data,
+      });
+    }
   });
 
-  // Si la app está en segundo plano o cerrada, esta función se activa
+  // Manejar notificaciones en segundo plano
   messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    console.log("Background Message:", remoteMessage.data);
+    console.log('Background Message:', remoteMessage);
+
+    // Mostrar notificación local
+    if (remoteMessage.notification) {
+      PushNotification.localNotification({
+        channelId: "default-channel",
+        title: remoteMessage.notification.title,
+        message: remoteMessage.notification.body,
+        data: remoteMessage.data,
+      });
+    }
   });
 };
