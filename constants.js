896cd0704ecb51e9cdaa39942c21d1844863a34b
theo jorge <theojorge2001@gmail.com>
2024-12-17 23:37:03 -0300

    Cambio hosting render url

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const GATEWAY_URL = "https://twitsnap-gateway-iq0i.onrender.com";
+export const GATEWAY_URL = "https://twitsnap-gateway-2wy0.onrender.com";
 export const RETRIES = 5;
 export const LIMIT = 10;
